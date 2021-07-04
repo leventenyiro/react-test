d60e99c841909789e12afb70ce435523ac9d6fea
Levente Nyírő <nyiro.levente@gmail.com>
2021-07-04 17:41:38 +0200

    add onclick

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log("Hello", e);
+    }
+
+    const handleClick2 = (name, e) => {
+        console.log(`Hello ${name}`);
+        console.log(e);
+    }
+    
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button><br />
+            <button onClick={(e) => handleClick2("Levi", e)}>Click me 2</button>
         </div>
     );
 }
