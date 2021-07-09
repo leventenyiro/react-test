2124649bcba3c488d9b8324226a463efe8f6d517
Levente Nyírő <nyiro.levente@gmail.com>
2021-07-09 21:52:37 +0200

    useEffect dependencies

--- src/Home.js
+++ src/Home.js
@@ -7,6 +7,8 @@ const Home = () => {
         { id: 2, title: "Rip", body: "Ripecske...", author: "Zsombor" }
     ]);
 
+    const [name, setName] = useState("Levi");
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
@@ -14,12 +16,14 @@ const Home = () => {
 
     useEffect(() => {
         console.log("Use effect ran");
-        console.log(blogs);
-    });
+        console.log(name);
+    }, [name]);
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            <button onClick={() => setName("Valami")}>Change name</button>
+            <p>{ name }</p>
         </div>
     );
 }
