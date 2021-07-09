30bd8d5a0f07d985ba16b11d146439deb0a0e1cd
Levente Nyírő <nyiro.levente@gmail.com>
2021-07-09 12:09:22 +0200

    reusing components

--- src/BlogList.js
+++ src/BlogList.js
@@ -6,13 +6,13 @@ const BlogList = ({ blogs, title }) => {
     return (
         <div className="blog-list">
             <h2>{ title }</h2>
-            {blogs.map((blog) => (
+            {blogs.map((blog) => 
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <hr />
                 </div>
-            ))}
+            )}
         </div>
     );
 }
