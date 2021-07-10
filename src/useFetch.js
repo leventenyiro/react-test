8db15ddfb9ec13a06e23810c488c533f062070b2
Levente Nyírő <nyiro.levente@gmail.com>
2021-07-10 11:49:26 +0200

    useEffect cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,22 +6,32 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
     
     useEffect(()=> {
-        fetch(url)
-        .then(res => {
-            if (!res.ok) {
-                throw Error("Could not fetch the data from that resource");
-            }
-            return res.json();
-        })
-        .then((data) => {
-            setData(data);
-            setIsPending(false);
-            setError(null);
-        })
-        .catch((err) => {
-            setError(err.message);
-            setIsPending(false);
-        });
+        const abortCont = new AbortController();
+
+        setTimeout(() => {
+            fetch(url, { signal: abortCont.signal })
+            .then(res => {
+                if (!res.ok) {
+                    throw Error("Could not fetch the data from that resource");
+                }
+                return res.json();
+            })
+            .then((data) => {
+                setData(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch((err) => {
+                if (err.name === 'AbortError')
+                    console.log("fetch aborted");
+                else {
+                    setError(err.message);
+                    setIsPending(false);
+                }
+            });
+        }, 1000);
+
+        return () => abortCont.abort();
     }, [url]);
 
 
