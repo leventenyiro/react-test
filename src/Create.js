1050b7b439496ff0438b4f1cff380c96338ffeb7
Levente Nyírő <nyiro.levente@gmail.com>
2021-07-10 12:19:32 +0200

    Controlled inputs (forms)

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState();
+    const [body, setBody] = useState();
+    const [author, setAuthor] = useState('levi');
+
     return (
         <div className="create">
             <h2>Add new blog</h2>
+            <form>
+                <label>Blog title:</label>
+                <input 
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <label>Blog body:</label>
+                <textarea
+                    required
+                    value={body}
+                    d       onChange={(e) => setBody(e.target.value)}        
+                ></textarea>
+
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="levi">Levi</option>
+                    <option value="zsombor">Zsombor</option>
+                </select>
+                <button>Add blog</button>
+                <p>{title}</p>
+            </form>
         </div>
     );
 }
